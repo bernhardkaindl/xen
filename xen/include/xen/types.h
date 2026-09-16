7e295a9d1665891b29e10e82d03c61fbc829635d
Bernhard Kaindl <bernhard.kaindl@citrix.com>
2026-09-16 12:00:00 +0200

    tests/native: Add native tests for NUMA claim sets
    Test the page allocator's claim behaviour using:
    
    - the legacy xc_domain_claim_pages() call, and
    - the new xc_domain_claim_memory() call of the claim-set API,
    
    exercising both host-wide and NUMA node-specific multi-node claims.
    
    Signed-off-by: Bernhard Kaindl <bernhard.kaindl@citrix.com>

--- xen/include/xen/types.h
+++ xen/include/xen/types.h
@@ -50,12 +50,24 @@ typedef __UINTPTR_TYPE__ uintptr_t;
 #define UINT64_MAX      (18446744073709551615ULL)
 #endif
 
+#ifndef INT_MAX
 #define INT_MAX         ((int)(~0U>>1))
+#endif
+#ifndef INT_MIN
 #define INT_MIN         (-INT_MAX - 1)
+#endif
+#ifndef UINT_MAX
 #define UINT_MAX        (~0U)
+#endif
+#ifndef LONG_MAX
 #define LONG_MAX        ((long)(~0UL>>1))
+#endif
+#ifndef LONG_MIN
 #define LONG_MIN        (-LONG_MAX - 1)
+#endif
+#ifndef ULONG_MAX
 #define ULONG_MAX       (~0UL)
+#endif
 
 typedef uint16_t __le16;
 typedef uint16_t __be16;
