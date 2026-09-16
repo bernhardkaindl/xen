bbef0fc4a0b27424709502d41513f0424e97092e
Bernhard Kaindl <bernhard.kaindl@citrix.com>
2026-09-16 12:00:00 +0200

    */asm/types.h: Guard typedef register_t/ssize_t with ifdef __XEN__
    Signed-off-by: Bernhard Kaindl <bernhard.kaindl@citrix.com>

--- xen/include/xen/types.h
+++ xen/include/xen/types.h
@@ -14,7 +14,9 @@ typedef uint64_t u64;
 
 typedef __SIZE_TYPE__ size_t;
 
+#ifdef __XEN__
 typedef signed long ssize_t;
+#endif
 
 typedef __PTRDIFF_TYPE__ ptrdiff_t;
 typedef __UINTPTR_TYPE__ uintptr_t;
