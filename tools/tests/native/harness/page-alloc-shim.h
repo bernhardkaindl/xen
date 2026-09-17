9ecd6a6f5a99f1bcd0022689dd2007e8b037bef1
Bernhard Kaindl <bernhard.kaindl@citrix.com>
2026-09-17 14:46:59 +0200

    #define get_order_from_pages(nr)          0U

--- tools/tests/native/harness/page-alloc-shim.h
+++ tools/tests/native/harness/page-alloc-shim.h
@@ -147,7 +147,6 @@ unsigned long memnodemapsize = sizeof(_memnodemap);
 #define softirq_pending(cpu)              false
 #define process_pending_softirqs()        ((void)0)
 #define on_selected_cpus(msk, f, data, w) ((void)0)
-#define get_order_from_pages(nr)          0U
 
 /* Testing hypercall preemption is not supported yet. */
 #undef hypercall_preempt_check
