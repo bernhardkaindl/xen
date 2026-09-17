b497f29239b53b22b9cba2c4453f7098d9cb8d07
Bernhard Kaindl <bernhard.kaindl@citrix.com>
2026-09-17 16:19:46 +0200

    page-alloc-env.h:     testcase_assert_verbose_assertions = false;

--- tools/tests/native/harness/page-alloc-env.h
+++ tools/tests/native/harness/page-alloc-env.h
@@ -240,7 +240,6 @@ unsigned long get_free_pages(const char *file, int line, const char *func)
 {
     bool verbose = testcase_assert_verbose_assertions;
 
-    testcase_assert_verbose_assertions = false;
     check_xc_avail_heap(file, line, func);
     testcase_assert(avail_heap_pages(MEMZONE_XEN, NR_ZONES - 1, -1) ==
                     total_avail_pages, file, line, func, "avail_heap_pages()");
