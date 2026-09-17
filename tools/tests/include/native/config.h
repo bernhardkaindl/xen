8265bff8ace8d43989d5edb24f913f53557205c5
Bernhard Kaindl <bernhard.kaindl@citrix.com>
2026-09-17 12:00:00 +0200

    tools/tests: Add configuration header for natively compiled tests
    Signed-off-by: Bernhard Kaindl <bernhard.kaindl@citrix.com>

---
+++ tools/tests/include/native/config.h
@@ -0,0 +1,29 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/* Configuration for natively compiled unit tests. */
+
+#include <assert.h>
+#include <errno.h>
+#include <stdio.h>
+
+#define CONFIG_DEBUG
+#define CONFIG_NR_NUMA_NODES 64
+#define CONFIG_NR_CPUS       128
+#define CONFIG_MMU
+#ifdef __arm__
+#define CONFIG_PADDR_BITS 40
+#endif
+#ifdef __aarch64__
+#define CONFIG_PADDR_BITS 48
+#endif
+#ifdef __riscv
+#define CONFIG_RISCV_64
+#endif
+
+#define __XEN_KCONFIG_H
+#define __XEN_PDX_H__
+#define __XEN_FRAME_NUM_H__
+#include <xen/config.h>
+#include <xen/pfn.h>
+#include <xen/sections.h>
+#include <xen/types.h>
+#pragma GCC visibility pop
